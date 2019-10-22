2d125123d027a2168e6099e241bef867e9ab6d6f
Christian Autermann <c.autermann@52north.org>
2019-10-22 11:52:28 +0200

    made config file location configurable

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,6 +6,7 @@ CONFIG_FILE='/etc/ckan-proxy.json'
 cat ${CONFIG_FILE} |\
     jq '.whitelist.ckan.url = "'${EDP_DATA_API_URL}'"' |\
     jq '.whitelist.ckan.updateInterval = '${WHITELIST_UPDATE_INTERVAL_MINUTES} |\
-    jq '.logging.level = "'${LOGGING_LEVEL}'"' >> ${CONFIG_FILE}.tmp && mv ${CONFIG_FILE}.tmp ${CONFIG_FILE}
+    jq '.logging.level = "'${LOGGING_LEVEL}'"' >> ${CONFIG_FILE}.tmp \
+  && mv ${CONFIG_FILE}.tmp ${CONFIG_FILE}
 
-ckan-proxy /etc/ckan-proxy.json
+ckan-proxy ${CONFIG_FILE}
